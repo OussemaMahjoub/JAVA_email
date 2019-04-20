de79a3501a1a274b671cea979d81f980610c6a20
OussemaMahjoub <47174218+OussemaMahjoub@users.noreply.github.com>
2019-04-20 03:14:43 +0100

    first

---
+++ src/email/Email.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package email;
+
+/**
+ *
+ * @author mahjoub
+ */
+public class Email {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new mailing("ouma510@gmail.com", "haha", "hihi");
+    }
+    
+}
